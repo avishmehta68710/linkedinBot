d1099064d0f584e3f94aa6ade7a3ef30df89b6da
avish mehta <avish@logicwind.co>
2021-07-27 20:33:54 +0400

    update

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-username = "YOUR_LINKEDIN_USERNAME"
-password = "YOUR_LINKEDIN_PASSWORD"
+username = "avishmehta2001@gmail.com"
+password = "jee68710rank"
 
 # your dream companies list, you can add more companies also
-companies_list = ["amazon","microsoft","apple","google","facebook"]
+companies_list = ["apna"]
 
-message = "YOUR CUSTOM MESSAGE "
+message = "Hi \n I am Avish Mehta and I am actively looking for the internships. I have done two internships and some projects and have a good hold in competitive programming and even published 2 papers. Please find my attached resume \n Resume - https: // bit.ly/3eTTZSZ \n Thanks "
--- stage3.py
+++ stage3.py
@@ -3,22 +3,24 @@ from login import *
 import config
 
 df = pd.read_csv("roles_of_person_in_pervious_list.csv")
-df.drop_duplicates(keep=False,inplace=True)
+df.drop_duplicates(keep=False, inplace=True)
 arr = list(df['Company_Name'])
 names = list(df['Name'])
-hitWords = ['full-time','part-time','internship','contract','crio.Do','girlscript foundation','girlscript summer of code','google summer of code','major league hacking','placementunit|bitspilani','highcommissionofcanadainindia','australiandepartmentofhomeaffairs','googlesummerofcode','majorleaguehacking','australiandepartmentofhomeaffairs','highcommissionofcanadainindia','britishrirways','resumevogue']
-blockWords = ['placement','university','bachleors','college','institute','jecrc','daiict','student','ieee','dtu','self-employed','self','iit','da-iict','army','school','corona','mit','harvard','freelancing','freelancer','freelance','youtuber']
+hitWords = ['full-time', 'part-time', 'internship', 'contract', 'crio.Do', 'girlscript foundation', 'girlscript summer of code', 'google summer of code', 'major league hacking', 'placementunit|bitspilani',
+            'highcommissionofcanadainindia', 'australiandepartmentofhomeaffairs', 'googlesummerofcode', 'majorleaguehacking', 'australiandepartmentofhomeaffairs', 'highcommissionofcanadainindia', 'britishrirways', 'resumevogue']
+blockWords = ['placement', 'university', 'bachleors', 'college', 'institute', 'jecrc', 'daiict', 'student', 'ieee', 'dtu', 'self-employed',
+              'self', 'iit', 'da-iict', 'army', 'school', 'corona', 'mit', 'harvard', 'freelancing', 'freelancer', 'freelance', 'youtuber']
 people = set()
 for j in range(len(arr)):
     designation = " ".join(arr[j].split())
     flag = False
-    if len(designation)>12:
+    if len(designation) > 12:
         if designation[0:12].lower() == "company name":
             flag = True
-            designation = designation.replace(designation[0:12],"")
-    if flag == False and len(designation)>7:
+            designation = designation.replace(designation[0:12], "")
+    if flag == False and len(designation) > 7:
         if (designation[0:7].lower() == "company"):
-            designation = designation.replace(designation[:7],"")
+            designation = designation.replace(designation[:7], "")
     designation = designation.lower().split()
     checker = False
     for i in range(len(designation)):
@@ -33,40 +35,48 @@ for j in range(len(arr)):
             people.add(" ".join(designation))
 people = list(people)
 count = 0
-for j in range(len(people)):
-    if count>99:
+for j in range(1):
+    if count > 99:
         break
-    driver.get("https://www.linkedin.com/search/results/companies/?keywords="+str(people[j])+"&origin=GLOBAL_SEARCH_HEADER")
+    driver.get("https://www.linkedin.com/search/results/companies/?keywords=" +
+               str(people[j])+"&origin=GLOBAL_SEARCH_HEADER")
     src = driver.page_source
-    parser = soup(src,"html.parser")
-    userList = parser.find_all("li",{"class":"reusable-search__result-container"})
+    parser = soup(src, "html.parser")
+    userList = parser.find_all(
+        "li", {"class": "reusable-search__result-container"})
     if len(userList) != 0:
         links = userList[0].a['href']+"/people/"
         driver.get(links)
         time.sleep(10)
         src = driver.page_source
-        parser = soup(src,"html.parser")
+        parser = soup(src, "html.parser")
         time.sleep(4)
-        user = parser.find_all("li",{"class":"grid grid__col--lg-8 pt5 pr4 m0"})
+        user = parser.find_all(
+            "li", {"class": "grid grid__col--lg-8 pt5 pr4 m0"})
         count = 0
         for i in range(len(user)):
             count += 1
             time.sleep(2)
             src = driver.page_source
-            parser = soup(src,"html.parser")
-            WebDriverWait(driver,1000)
+            parser = soup(src, "html.parser")
+            WebDriverWait(driver, 1000)
             try:
-                connectRequest = driver.find_element_by_xpath("//button[@class='artdeco-button artdeco-button--2 artdeco-button--secondary ember-view full-width']")
+                connectRequest = driver.find_element_by_xpath(
+                    "//button[@class='artdeco-button artdeco-button--2 artdeco-button--secondary ember-view full-width']")
                 if connectRequest:
                     connectRequest = connectRequest.click()
-                    addNote = driver.find_element_by_xpath("//button[@class='mr1 artdeco-button artdeco-button--muted artdeco-button--3 artdeco-button--secondary   ember-view']")
+                    addNote = driver.find_element_by_xpath(
+                        "//button[@class='mr1 artdeco-button artdeco-button--muted artdeco-button--3 artdeco-button--secondary ember-view']")
                     time.sleep(1)
                     addNote.click()
-                    message = driver.find_element_by_xpath("//textarea[@class='ember-text-area ember-view connect-button-send-invite__custom-message mb3']")
-                    name = driver.find_element_by_xpath("//div[@class='org-people-profile-card__profile-title t-black lt-line-clamp lt-line-clamp--single-line ember-view']").text
+                    message = driver.find_element_by_xpath(
+                        "//textarea[@class='ember-text-area ember-view connect-button-send-invite__custom-message mb3']")
+                    name = driver.find_element_by_xpath(
+                        "//div[@class='org-people-profile-card__profile-title t-black lt-line-clamp lt-line-clamp--single-line ember-view']").text
                     print(name)
                     message.send_keys(config.message)
-                    send = driver.find_element_by_xpath("//button[@class='ml1 artdeco-button artdeco-button--3 artdeco-button--primary ember-view']")
+                    send = driver.find_element_by_xpath(
+                        "//button[@class='ml1 artdeco-button artdeco-button--3 artdeco-button--primary ember-view']")
                     send.click()
                 else:
                     pass
